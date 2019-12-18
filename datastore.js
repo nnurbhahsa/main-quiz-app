e71f06764011641743c14717eee4c5b156ec45f1
Nihal Nurbhasha <Nihal@Nihals-MacBook-Pro.local>
2019-12-17 18:28:34 -0600

    initial

---
+++ datastore.js
@@ -0,0 +1,102 @@
+const STORE = [
+    {
+      question: 'The Paleolitic era is also known as the ',
+      answers: [
+        'Old Stone Age.',
+        'New Stone Age.',
+        'Bronze Age.',
+        'Iron Age.'
+        ],
+      correctAnswer: 'Old Stone Age.'
+    },
+    {
+      question: 'The Old Stone Age is from ',
+      answers: [
+        '3.4 million years ago to 2.5 million years ago.',
+        '2.5 million years ago to the end of the last Ice Age.',
+        '2.5 million years ago to 3,300 BCE (farming)',
+        'the end of the last Ice Age to 4000 BCE.'
+        ],
+        correctAnswer: '2.5 million years ago to the end of the last Ice Age.'
+    },
+    {
+      question: 'The Paleolithic era is characterized by ',
+      answers: [
+        'farming',
+        'agriculture',
+        'stone tools',
+        'cave paintings'
+        ],
+        correctAnswer: 'stone tools'
+    },
+    {
+      question: 'Which, of the genus Homo, attained Paleolithic development?',
+      answers: [
+        'Australopithecus anamensis and Homo sapiens',
+        'Homo neanderthalensis and Homo sapiens',
+        'Homo habilis and Homo sapiens',
+        'Homo erectus and Homo sapiens'
+        ],
+      correctAnswer: 'Homo neanderthalensis and Homo sapiens'
+    },
+    {
+      question: 'What economy was typical of Peleolithic society?',
+      answers: [
+        'capitalism',
+        'agriculture',
+        'communism',
+        'hunter-gatherer'
+        ],
+      correctAnswer: 'hunter-gatherer'
+    },
+    {
+      question: 'Paleolithic humans enjoyed ____ leisure time than those in farming and industial societies.',
+      answers: [
+        'more',
+        'less',
+        'the same',
+        '(unknown)'
+        ],
+      correctAnswer: 'more'
+    },
+    {
+      question: 'People in the Paleolithic were nomadic because ',
+      answers: [
+      'they lacked the knowledge to build sturdy houses.',
+      'they were bored and wanted to see the world.',
+      'they followed herds of animals to aquire food.',
+      'it was advatageous relative to other hominins.'
+      ],
+      correctAnswer: 'they followed herds of animals to aquire food.'
+    },
+    {
+      question: 'Homo neanderthalensis were extinct by the end of the Paleolitic but we have traces of their DNA because ',
+      answers: [
+        'we are their descendants.',
+        'we interbred while coexisting.',
+        'there were similar genetic mutations.',
+        'of God; He decided to.'
+      ],
+      correctAnswer: 'we interbred while coexisting.'
+    },
+    {
+      question: 'Evidence for the widespread use of language by humans during the Paleolithic includes ',
+      answers: [
+      'evidence of collaboration between different groups.',
+      'various written inscriptions found at Paleolithic sites.',
+      'long-distance travel and trade, and organizational structures.',
+      'the ability to hunt animals for food and then share it.'
+      ],
+      correctAnswer: 'long-distance travel and trade, and organizational structures.'
+    },
+    {
+      question: 'What marked the end of the Paleolithic era and the beginning of the Mesolithic era?',
+      answers: [
+        'The beginning of agriculture.',
+        'The extinction of most humans.',
+        'The introduction of complex tools.',
+        'The end of the last Ice Age.'
+    ],
+    correctAnswer: 'The end of the last Ice Age.'
+    }
+];
---
+++ index.js
@@ -0,0 +1,138 @@
+let quesNum = 0;
+let score = 0;
+
+// increment score
+function changeScore () {
+    score ++;
+}
+
+// increment question number
+function changeQues () {
+    quesNum ++;
+    $('.quesNum').text(quesNum+1);
+}
+
+// to start quiz
+function startQuiz () {
+    $('.quizStart').on('click', '.startButton', function (event) {
+        $('.quizStart').remove();
+        $('.qAform').css('display', 'block');
+        $('.quesNum').text(1);
+    });
+}
+
+// how question will be displayed
+function quesDisp () {
+    if (quesNum < STORE.length) {
+        return `<section class="question-${quesNum}">
+        <h2>${STORE[quesNum].question}</h2>
+            <form>
+                <fieldset>
+                    <label>
+                        <input type="radio" value="${STORE[quesNum].answers[0]}" name="answer" required>
+                            <span>${STORE[quesNum].answers[0]}</span>
+                    </label>
+                    <label>
+                        <input type="radio" value="${STORE[quesNum].answers[1]}" name="answer" required>
+                            <span>${STORE[quesNum].answers[1]}</span>
+                    </label>
+                    <label>
+                        <input type="radio" value="${STORE[quesNum].answers[2]}" name="answer" required>
+                            <span>${STORE[quesNum].answers[2]}</span>
+                    </label>
+                    <label>
+                        <input type="radio" value="${STORE[quesNum].answers[3]}" name="answer" required>
+                            <span>${STORE[quesNum].answers[3]}</span>
+                    </label>
+                    <button type="submit" class="submitButton">Submit</button>
+                </fieldset>
+            </form>
+            </section>`;
+    } else {
+        resultsPage();
+        restartQuiz();
+        $('.quesNum').text(10)
+    }
+}
+
+// next question display
+function newQues () {
+    $('.qAform').html(quesDisp());
+}
+
+// when selecting answer
+function selectAnswer () {
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        
+        let selected = $('input:checked');
+        let answer = selected.val();
+        let correctAnswer = `${STORE[quesNum].correctAnswer}`;
+        
+        if (answer === correctAnswer) {
+            selected.parent().addClass('correct');
+            rightAns();
+        } else {
+            selected.parent().addClass('wrong');
+            wrongAns();
+        }
+    });
+}
+
+function wrongAns () {
+    wrongFeedback();
+}
+
+function rightAns () {
+    rightFeedback();
+    updateScore();
+}
+
+function wrongFeedback () {
+    let correctAnswer = `${STORE[quesNum].correctAnswer}`;
+    $('.qAform').html(`<section class="correctFeedback"><p><b>INCORRECT</b><br>correct answer: <span>"${correctAnswer}"</span></p><button type=button class="nextButton">Next</button></section>`);
+}
+
+function rightFeedback () {
+    let correctAnswer = `${STORE[quesNum].correctAnswer}`;
+    $('.qAform').html(`<section class="correctFeedback"><p><b>CORRECT</b></p><button type=button class="nextButton">Next</button></section>`);
+}
+
+function updateScore () {
+    changeScore();
+    $('.score').text(score);
+}
+
+function resultsPage () {
+    if (score >= 8) {
+    $('.qAform').html(`<section class="results correctFeedback"><h3>Great job!</h3><p>You got ${score} / 10</p>><button class="restartButton">Restart Quiz</button></section>`);
+    } else if (score < 8 && score >= 5) {
+    $('.qAform').html(`<section class="results correctFeedback"><h3>Almost there!</h3><p>You got ${score} / 10</p><button class="restartButton">Restart Quiz</button></section>`);
+    } else {
+    $('.qAform').html(`<section class="results correctFeedback"><h3>Nice try!</h3><p>You got ${score} / 10</p><button class="restartButton">Restart Quiz</button></section>`);
+    }
+}
+
+function nextQuestion () {
+    $('main').on('click', '.nextButton', function (event) {
+        changeQues();
+        newQues();
+        selectAnswer();
+    });
+}
+
+function restartQuiz () {
+    $('main').on('click', '.restartButton', function (event) {
+        location.reload();
+    });
+}
+
+// calling all quiz functions
+function createQuiz () {
+    startQuiz();
+    newQues();
+    selectAnswer();
+    nextQuestion();
+}
+
+$(createQuiz);
